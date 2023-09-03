a310712999a23154c7e873a06a2f77d15948d234
fabbro <fabbro@infograb.net>
2023-09-03 12:47:12 +0900

    🔥Header 스크롤 시 글씨색 햐안색으로 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,32 @@
+"use client";
+
 import { Diphylleia } from "next/font/google";
 import Link from "next/link";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
-const diphylleia = Diphylleia({ subsets: ["latin"], weight: ["400"] });
+const DEFAULT_STYLE = "";
+const SCROLL_STYLE = "text-white";
+const DIPHYLLEIA = Diphylleia({ subsets: ["latin"], weight: ["400"] });
 
 function Header() {
+  const [headerStyle, setHeaderStyle] = useState(DEFAULT_STYLE);
+
+  const handleScroll = useCallback(() => {
+    if (window.scrollY < 40) {
+      setHeaderStyle(DEFAULT_STYLE);
+    } else {
+      setHeaderStyle(SCROLL_STYLE);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
   return (
     <header
-      className={`${diphylleia.className} sticky top-0 flex justify-between items-center p-4 z-50`}
+      className={`${DIPHYLLEIA.className} ${headerStyle} sticky top-0 flex justify-between items-center p-4 z-50 duration-300 ease-in`}
     >
       <Link href="/">
         <h1 className="text-3xl font-bold">{"파브로 대장간"}</h1>
