69cb6068435e6662b452f6118a18363a1f3668a9
fabbro <fabbro@infograb.net>
2023-09-03 13:02:57 +0900

    🔥페이지 추가: armory, master, posts

---
+++ src/app/armory/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ArmoryPage() {
+  return <div>ArmoryPage</div>;
+}
+
+export default ArmoryPage;
---
+++ src/app/master/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MasterPage() {
+  return <div>MasterPage</div>;
+}
+
+export default MasterPage;
