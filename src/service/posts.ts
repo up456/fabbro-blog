263d4ba26b2ba16be781d245788ca2da4a32a870
용석 박 <fabbro@infograb.net>
2023-09-17 18:16:01 +0900

    🔥 Blog관련 type 선언 및 지정

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -2,37 +2,27 @@ import { metadata } from "./../app/layout";
 import { readFile } from "fs/promises";
 import path from "path";
 
-export type Post = {
-  title: string;
-  description: string;
-  date: string;
-  category: string[];
-  featured: boolean;
-};
-
-export type PostData = Post & { content: string };
-
-export async function getFeaturedPosts(): Promise<Post[]> {
+export async function getFeaturedPosts(): Promise<BlogMetadata[]> {
   return getAllPosts() //
     .then((posts) => posts.filter((post) => post.featured));
 }
 
-export async function getNonFeaturedPosts(): Promise<Post[]> {
+export async function getNonFeaturedPosts(): Promise<BlogMetadata[]> {
   return getAllPosts() //
     .then((posts) => posts.filter((post) => !post.featured));
 }
 
-export async function getAllPosts(): Promise<Post[]> {
+export async function getAllPosts(): Promise<BlogMetadata[]> {
   const filePath = path.join(process.cwd(), "data", "metadata.json");
   return readFile(filePath, "utf-8")
-    .then<Post[]>(JSON.parse)
+    .then<BlogMetadata[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
 }
 
 export async function getPostData(
   fileName: string,
   postYear: string
-): Promise<PostData> {
+): Promise<BlogMetadataWithContent> {
   const filePath = path.join(
     process.cwd(),
     "data",
