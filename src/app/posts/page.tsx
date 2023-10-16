b8538bf70b9f57de510b4912c2411ccccbcfee57
용석 박 <fabbro@infograb.net>
2023-10-16 23:02:00 +0900

    🔨Fix: 카테고리 로직이 잘 못되어 블로그가 카테고리 별로 필터링 되지 않는 버그 수정
    - state명 조금더 명확하게 수정

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,6 +3,6 @@ import { getAllPosts } from "@/service/posts";
 
 export default async function PostsPage() {
   const posts = await getAllPosts();
-  const categories = [...new Set(posts.map((post) => post.category))];
+  const categories = [...new Set(posts.map((post) => post.category).flat())];
   return <FilterablePosts posts={posts} categories={categories} />;
 }
