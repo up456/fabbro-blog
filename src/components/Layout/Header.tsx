07e475e81a204064946a29e64bc4798376bee728
용석 박 <fabbro@infograb.net>
2024-02-04 18:44:43 +0900

    ✨[작업]: 기본페이지 잠금

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -37,9 +37,9 @@ function Header() {
         <h1 className="text-3xl font-bold">{"파브로 대장간"}</h1>
       </Link>
       <nav className="flex gap-4">
-        <Link href="/master">주인장</Link>
-        <Link href="/armory">무기고</Link>
-        <Link href="/posts">게시판</Link>
+        {/* <Link href="/master">주인장</Link> */}
+        {/* <Link href="/armory">무기고</Link> */}
+        {/* <Link href="/posts">게시판</Link> */}
       </nav>
     </header>
   );
